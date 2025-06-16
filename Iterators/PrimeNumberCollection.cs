24d6c1ab2592039fed65cd569a31c06c9ec47359
Sachin Kotapure <dev.sachein@yahoo.com>
2025-06-16 13:23:08 +0530

    Iterators example

---
+++ Iterators/PrimeNumberCollection.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterators
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class PrimeNumberCollection : IEnumerable<int>
+    {
+        private int start;
+        private int end;
+
+        public PrimeNumberCollection(int start, int end)
+        {
+            if (start > end)
+                throw new ArgumentException("Start must be less than or equal to end");
+
+            this.start = start;
+            this.end = end;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = start; i <= end; i++)
+            {
+                if (IsPrime(i))
+                {
+                    yield return i;
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private bool IsPrime(int number)
+        {
+            if (number <= 1) return false;
+            if (number == 2) return true;
+
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0) return false;
+            }
+
+            return true;
+        }
+    }
+
+}
---
+++ Iterators/Program.cs
@@ -0,0 +1,15 @@
+﻿using Iterators;
+
+class Program
+{
+    static void Main()
+    {
+        PrimeNumberCollection primes = new PrimeNumberCollection(10, 30);
+
+        Console.WriteLine("Prime numbers between 10 and 30:");
+        foreach (int prime in primes)
+        {
+            Console.WriteLine(prime);
+        }
+    }
+}
