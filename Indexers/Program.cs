e1383c0a35a2aa48cb2a71a42be85874f79fe328
Sachin Kotapure <dev.sachein@yahoo.com>
2025-06-16 13:18:04 +0530

    Added Indexers example

--- Indexers/Program.cs
+++ Indexers/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Indexers;
+
+class Program
+{
+    static void Main()
+    {
+        WeekSchedule schedule = new WeekSchedule();
+
+        schedule["Monday"] = "Team meeting";
+        schedule["Wednesday"] = "Project work";
+        schedule["Friday"] = "Code review";
+
+        Console.WriteLine("Monday: " + schedule["Monday"]);
+        Console.WriteLine("Wednesday: " + schedule["Wednesday"]);
+        Console.WriteLine("Friday: " + schedule["Friday"]);
+
+        // Trying an invalid day
+        try
+        {
+            Console.WriteLine(schedule["Funday"]);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+    }
+}
---
+++ Indexers/WeekSchedule.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexers
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class WeekSchedule
+    {
+        private readonly Dictionary<string, string> tasks = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+    {
+        { "Monday", "" },
+        { "Tuesday", "" },
+        { "Wednesday", "" },
+        { "Thursday", "" },
+        { "Friday", "" },
+        { "Saturday", "" },
+        { "Sunday", "" }
+    };
+
+        // Indexer
+        public string this[string day]
+        {
+            get
+            {
+                if (tasks.ContainsKey(day))
+                    return tasks[day];
+                else
+                    throw new ArgumentException("Invalid day: " + day);
+            }
+
+            set
+            {
+                if (tasks.ContainsKey(day))
+                    tasks[day] = value;
+                else
+                    throw new ArgumentException("Invalid day: " + day);
+            }
+        }
+    }
+
+}
