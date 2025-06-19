75fd05ca3085d8817c63fd3aba6b037d846a8646
Sachin Kotapure <dev.sachein@yahoo.com>
2025-06-19 11:38:58 +0530

    SQL Queries demo

---
+++ MSSQL_Database/FindSQLVersion.sql
@@ -0,0 +1,16 @@
+﻿
+
+SELECT SERVERPROPERTY('productversion'), SERVERPROPERTY ('productlevel'), SERVERPROPERTY ('edition')
+
+
+--Output : 16.0.1135.2 |	RTM  |	Developer Edition (64-bit)
+
+SELECT @@VERSION
+
+--Output : Microsoft SQL Server 2022 (RTM-GDR) (KB5046861) - 16.0.1135.2 (X64)
+
+
+
+
+
+
