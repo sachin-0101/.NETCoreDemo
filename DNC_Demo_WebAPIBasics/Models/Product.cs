3a14fc96a7cdda4c92c0fc498eee1fb6b86271dc
Sachin Kotapure <dev.sachein@yahoo.com>
2025-06-23 14:43:55 +0530

    added product class

---
+++ DNC_Demo_WebAPIBasics/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DNC_Demo_WebAPIBasics.Models
+{
+    [Table("tblProducts")]
+    public class Product
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Product name is required.")]
+        [StringLength(50, MinimumLength =3)]
+        public string? Name { get; set; }
+        [MaxLength(50)]
+        public string? Description { get; set; }
+
+        [DataType(DataType.Currency)]
+        public int Price { get; set; }
+    }
+}
